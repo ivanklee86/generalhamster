b7912cc751504a05a1315b13c0bf2e4b70d7c148
Ivan Lee <ivanklee86@gmail.com>
2019-02-07 09:15:09 -0500

    Initial setup.

--- app/plugins/status/plugin.py
+++
@@ -1,8 +0,0 @@
-import re
-from slackbot.bot import respond_to
-from app.constants import VERSION
-
-
-@respond_to('status', re.IGNORECASE)
-def status(message):
-    message.reply(f"Hello!  I am a slackbot, version {VERSION}")
